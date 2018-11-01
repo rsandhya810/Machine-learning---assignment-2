40f2d90ba5f4939b86f1f113ecb69c44d310fcf6
Sandhya Aarathi Ramesh Babu <35511853+rsandhya810@users.noreply.github.com>
2018-11-01 16:00:28 -0500

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -21,8 +21,8 @@ df_ss1 = df.median()
 df_ss1
 
 #To find the count of missing/Nan values
-df_ss2 = df.count()
-df_ss2
+df_ss2 = df.isnull().sum()
+df_ss2.shape
 
 #To drop the column with more than 50% missings
 df_2 = df_ss.loc[:, df.isnull().sum() < 0.5*df.shape[0]]
@@ -41,3 +41,11 @@ with open('/Users/ramesh/Desktop/data.csv') as inf:
     print(tabulate([(row[0], row[1], row[2]-row[3],
                      "{:>2} : {:2}".format(row[2], row[3])) for row in reader],
                    headers=headers))
+    
+df.describe(include='all').style
+
+def color(val):
+    color = 'red' if val < 0 else 'green'
+    return 'color: %s' % color
+table = df.describe(include='all').style.applymap(color)
+table
